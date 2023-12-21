e086c49867428040809bfe1d42779931e0ef43cc
zpigula <zpigula@gmail.com>
2023-12-20 22:32:27 -0600

    Add zbot_joy package
    twist mux and joystick support

---
+++ ws_joy/src/zbot_joy/README.md
@@ -0,0 +1,1 @@
+## Twist mux / xbox package
---
+++ ws_joy/src/zbot_joy/launch/joystick.launch.py
@@ -0,0 +1,53 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    joy_params = os.path.join(get_package_share_directory('zbot_joy'),'config','joystick.yaml')
+    twist_mux_params = os.path.join(get_package_share_directory('zbot_joy'),'config','twist_mux.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+         )
+
+    # teleop_node = Node(
+    #         package='teleop_twist_joy',
+    #         executable='teleop_node',
+    #         name='teleop_node',
+    #         parameters=[joy_params, {'use_sim_time': use_sim_time}],
+    #         remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+    #      )
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}],
+        remappings=[('/cmd_vel','/cmd_vel_joy')]
+        )
+
+    twist_mux = Node(
+            package='twist_mux',
+            executable='twist_mux',
+            parameters=[twist_mux_params, {'use_sim_time': use_sim_time}],
+            #remappings=[('/cmd_vel_out','/diffbot_base_controller/cmd_vel_unstamped')]
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+         )
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='False',
+            description='Use sim time if true'),
+        joy_node,
+        teleop_node,
+        twist_mux       
+    ])
